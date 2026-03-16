e686b59286a498348546bfd4d4ea18ca29cba8bd
groomigroom <googjohn0143@gmail.com>
2026-03-16 22:13:52 +0000

    router_setting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header_part from "./component/Header_part";
 import DayList from './component/DayList';
 import Day from './component/Day';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
