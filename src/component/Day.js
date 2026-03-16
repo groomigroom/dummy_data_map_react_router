d2a4d7aa2c4a04b3d2cc81d1d684785da4da6312
groomigroom <googjohn0143@gmail.com>
2026-03-16 11:31:05 +0000

    trtdset

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,1 +1,18 @@
-export default function Day() { }
+import dummy from "../db/data.json";
+
+export default function Day() {
+    return (
+        <div>
+            <table>
+                <tbody>
+                    {words.map(word => ())}
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    );
+
+}
