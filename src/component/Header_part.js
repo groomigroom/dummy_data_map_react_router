8b6731cd54e5008e11cd93eeeb1b5a191ac05165
groomigroom <googjohn0143@gmail.com>
2026-03-16 05:02:52 +0000

    haerpartmenupart

--- src/component/Header_part.js
+++ src/component/Header_part.js
@@ -1,5 +1,13 @@
 export default function Header_part(){
     return (
-        <div className="header_part"></div>
+        <div className="header_part">
+            <h1>
+                <a href="/">토익 영단어(고급)</a>
+            </h1>
+            <div className="menu">
+                <a href="#x" className="link">단어 추가</a>
+                <a href="#x" className="link">단어 추가</a>
+            </div>
+        </div>
     );
 }
