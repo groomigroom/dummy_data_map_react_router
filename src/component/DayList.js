68a06c5c93eb6a9757a2108c95f577e88070cc16
groomigroom <googjohn0143@gmail.com>
2026-03-17 04:00:54 +0000

    ahref

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -6,6 +6,7 @@ export default function DayList() {
         <ul className='list_day'>
                 {dummy.days.map(day => (
                     <li key={day.id}>
+                        <a href=''></a>
                         Day {day.day}
                     </li>
                 ))} 
